960d6d50a60fd2b47f540930ad6986367f5821f5
Jonathan Prince <jonathan.prince@gmail.com>
2014-11-08 22:47:52 +0100

    add/remove names from online user list

--- index.js
+++ index.js
@@ -9,8 +9,25 @@ app.get('/', function(req, res){
 	res.sendFile('index.html');
 });
 
-io.on('connection', function(){
-  console.log('User connected');
+var loggedIn = [];
+
+io.on('connection', function(client){
+
+  client.emit('online users', loggedIn);
+
+  client.on('login', function(data){
+    client.user = data;
+    console.log('+' + client.user + ' logged in');
+    loggedIn.push(client.user);
+    io.emit('new user', client.user);
+  });
+
+  client.on('disconnect', function(){
+    var thisUserIndex = loggedIn.indexOf(client.user);
+    loggedIn.splice(thisUserIndex, 1);
+    io.emit('online users', loggedIn);
+  });
+
 });
 
 server.listen(8000, function(){
