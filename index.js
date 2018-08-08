ef38e3c35674ac3b9f4aaab3622106f856538ff7
Jonathan Prince <jonathan.prince@gmail.com>
2018-08-08 13:35:55 +0200

    lint using standardjs format

--- index.js
+++ index.js
@@ -1,78 +1,67 @@
-'use strict';
+'use strict'
 
-var express = require('express')
-  , app     = express()
-  , server  = require('http').createServer(app)
-  , io      = require('socket.io')(server)
-  , log     = require('./lib/log')
-  , msgs    = []
-  , users   = [];
+const path = require('path')
+const express = require('express')
+const app = express()
+const server = require('http').createServer(app)
+const io = require('socket.io')(server)
+const log = require('./lib/log')
+const msgs = []
+const users = []
 
 // declare path for static files
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(path.join(__dirname, '/public')))
 
 // serve chat ui on get request
-app.get('/', function(req, res){
-	res.sendFile('index.html');
-});
+app.get('/', (req, res) => res.sendFile('index.html'))
 
 // listen for new connection
-io.on('connection', function(client){
-
-  io.emit('online users', users);
+io.on('connection', client => {
+  io.emit('online users', users)
 
   // listen for username when logged in
-  client.on('login', function(data){
-    client.username = data;
-    users.push(client.username);
-    log('+ ' + client.username + ' logged in');
-    io.emit('new user', client.username);
-    msgs.forEach(function(msg){
-      client.emit('message', msg);
-    });
+  client.on('login', username => {
+    client.username = username
+    users.push(username)
+    log(`+ ${username} logged in`)
+    io.emit('new user', username)
+    msgs.forEach(msg => client.emit('message', msg))
     io.emit('message', {
-      name: client.username,
+      name: username,
       message: '... joined the party...'
-    });
-  });
+    })
+  })
 
   // listen for disconnect
-  client.on('disconnect', function(){
+  client.on('disconnect', () => {
     if (typeof client.username !== 'undefined') {
-      var userIndex = users.indexOf(client.username);
-      users.splice(userIndex, 1);
-      log('- ' + client.username + ' logged out');
-      io.emit('online users', users);
+      const userIndex = users.indexOf(client.username)
+      users.splice(userIndex, 1)
+      log(`- ${client.username} logged out`)
+      io.emit('online users', users)
 
       // send farewell message
       io.emit('message', {
         name: client.username,
         message: '... has left the building...'
-      });
+      })
     }
-  });
+  })
 
   // listen for new messages
-  client.on('new message', function(data){
-    io.emit('message', data);
-    log(data.name + ' : ' + data.message);
-    msgs.push(data);
+  client.on('new message', data => {
+    io.emit('message', data)
+    log(`${data.name} : ${data.message}`)
+    msgs.push(data)
     while (msgs.length > 10) {
-      msgs.shift();
+      msgs.shift()
     }
-  });
+  })
 
   // listen for typing
-  client.on('typing', function(data){
-    client.broadcast.emit('typing', data);
-  });
-  client.on('stop typing', function(data){
-    client.broadcast.emit('stop typing', data);
-  });
-
-});
+  client.on('typing', data => client.broadcast.emit('typing', data))
+  client.on('stop typing', data => client.broadcast.emit('stop typing', data))
+})
 
 // listen for requests on 8000
-server.listen(8000, function(){
-  console.log('Server listening on port 8000');
-});
+server.listen(8000, () => console.log('Server listening on port 8000'))
--- public/js/chat.js
+++ public/js/chat.js
@@ -1,136 +1,122 @@
-(function() {
+/* global io, sessionStorage, prompt */
+(function () {
+  var socket = io.connect()
+  var userList = document.getElementById('users')
+  var sendButton = document.getElementById('send')
+  var messages = document.getElementById('messages')
+  var textIn = document.getElementById('input')
+  var audio = document.getElementById('audio')
+  var user
 
-  var socket     = io.connect(),
-      userList   = document.getElementById('users'),
-      sendButton = document.getElementById('send'),
-      messages   = document.getElementById('messages'),
-      textIn     = document.getElementById('input'),
-      audio      = document.getElementById('audio'),
-      user;
-
-  var addNewUser = function(name) {
-    var item = document.createElement('li');
-    var txt = document.createTextNode(name);
-    item.appendChild(txt);
-    userList.appendChild(item);
-  };
-
-  var sendMessage = function(e) {
-    e.preventDefault();
-    var msg = textIn.value;
-    if (msg !== '') {
-      socket.emit('new message', {name: user, message: msg});
-      textIn.value = '';
-      if (typingSent === true) {
-        clearTimeout(timerId);
-        typingEnded();
-      }
-    }
-  };
-
-  var addMessage = function(data) {
-    var entry = document.createElement('div');
-    var txt = document.createTextNode(data.name + ': ' + data.message);
-    entry.appendChild(txt);
-    messages.appendChild(entry);
-    messages.scrollTop = messages.scrollHeight;
-    if (data.name !== user) {
-      audio.play();
-    }
-  };
-
-  var showTyping = function(name, state) {
-    var color = state ? 'red' : '#000';
-    var users = userList.childNodes;
-    for (var i = 0; i < users.length; i++) {
-      if (users[i].innerHTML === name) {
-        users[i].style.color = color;
-      }
-    }
-  };
-
-  var typingSent = false;
-  var timerId;
-
-  var typingEnded = function(){
-
-    socket.emit('stop typing', {name: user});
-    typingSent = false;
-  };
-
-  var keypressHandler = function(e) {
-
-    // send message on enter or send typing feedback
-    if(e.keyCode === 13){
-
-      sendMessage(e);
-
-    } else {
-
-      // check if user was already typing
-      if (typingSent === false) {
-
-        // send message that using is typing
-        socket.emit('typing', {name: user});
-        typingSent = true;
-        timerId = setTimeout(typingEnded, 5000);
-      } else {
-
-        // clear timer and restart
-        clearTimeout(timerId);
-        timerId = setTimeout(typingEnded, 5000);
-      }
-    }
-  };
-
-  if (sessionStorage.getItem("username") == 'null' ||
-      sessionStorage.getItem("username") == null) {
-
-    user = prompt('username?');
-
-    sessionStorage.setItem('username', user);
-
-  } else {
-
-    user = sessionStorage.getItem("username");
-
+  var addNewUser = function (name) {
+    var item = document.createElement('li')
+    var txt = document.createTextNode(name)
+    item.appendChild(txt)
+    userList.appendChild(item)
   }
 
-  socket.emit('login', user);
+  var sendMessage = function (e) {
+    e.preventDefault()
+    var msg = textIn.value
+    if (msg !== '') {
+      socket.emit('new message', {name: user, message: msg})
+      textIn.value = ''
+      if (typingSent === true) {
+        clearTimeout(timerId)
+        typingEnded()
+      }
+    }
+  }
+
+  var addMessage = function (data) {
+    var entry = document.createElement('div')
+    var txt = document.createTextNode(data.name + ': ' + data.message)
+    entry.appendChild(txt)
+    messages.appendChild(entry)
+    messages.scrollTop = messages.scrollHeight
+    if (data.name !== user) {
+      audio.play()
+    }
+  }
+
+  var showTyping = function (name, state) {
+    var color = state ? 'red' : '#000'
+    var users = userList.childNodes
+    for (var i = 0; i < users.length; i++) {
+      if (users[i].innerHTML === name) {
+        users[i].style.color = color
+      }
+    }
+  }
+
+  var typingSent = false
+  var timerId
+
+  var typingEnded = function () {
+    socket.emit('stop typing', {name: user})
+    typingSent = false
+  }
+
+  var keypressHandler = function (e) {
+    // send message on enter or send typing feedback
+    if (e.keyCode === 13) {
+      sendMessage(e)
+    } else {
+      // check if user was already typing
+      if (typingSent === false) {
+        // send message that using is typing
+        socket.emit('typing', {name: user})
+        typingSent = true
+        timerId = setTimeout(typingEnded, 5000)
+      } else {
+        // clear timer and restart
+        clearTimeout(timerId)
+        timerId = setTimeout(typingEnded, 5000)
+      }
+    }
+  }
+
+  if (sessionStorage.getItem('username') === 'null' ||
+      sessionStorage.getItem('username') === null) {
+    user = prompt('username?')
+
+    sessionStorage.setItem('username', user)
+  } else {
+    user = sessionStorage.getItem('username')
+  }
+
+  socket.emit('login', user)
 
   // event listeners for ui
-  sendButton.addEventListener('click', sendMessage, false);
-  textIn.addEventListener('keyup', keypressHandler, false);
+  sendButton.addEventListener('click', sendMessage, false)
+  textIn.addEventListener('keyup', keypressHandler, false)
 
   // listen for server sending list of users
-  socket.on('online users', function(data){
-
+  socket.on('online users', function (data) {
     while (userList.firstChild) {
-      userList.removeChild(userList.firstChild);
+      userList.removeChild(userList.firstChild)
     }
 
-    data.forEach(function(el){
-      addNewUser(el);
-    });
-
-  });
+    data.forEach(function (el) {
+      addNewUser(el)
+    })
+  })
 
   // listen for new user joining
-  socket.on('new user', function(data){
-    addNewUser(data);
-  });
+  socket.on('new user', function (data) {
+    addNewUser(data)
+  })
 
   // listen for messages
-  socket.on('message', function(data){
-    addMessage(data);
-  });
+  socket.on('message', function (data) {
+    addMessage(data)
+  })
 
   // listen for typing
-  socket.on('typing', function(data){
-    showTyping(data.name, true);
-  });
-  socket.on('stop typing', function(data){
-    showTyping(data.name, false);
-  });
-
-})();
+  socket.on('typing', function (data) {
+    showTyping(data.name, true)
+  })
+  socket.on('stop typing', function (data) {
+    showTyping(data.name, false)
+  })
+})()
